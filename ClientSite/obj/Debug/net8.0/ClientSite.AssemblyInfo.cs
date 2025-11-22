9d98c25db0721f0801392bd8d0c2bbb381a2034d
Hafiz <mdhafizul3131@gmail.com>
2025-11-22 14:04:03 +0600

    navbar Issue solved

--- ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
+++ ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26f0dedcca230a8213f31981cd6efb2d5798999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06cc715fa937d7259c412f3c5fce1c6b8032d0ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
