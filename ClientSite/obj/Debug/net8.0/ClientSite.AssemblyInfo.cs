8529d860b7c0adacf7d36fc1cd8b024b1cd2be68
Hafiz <mdhafizul3131@gmail.com>
2025-11-23 00:00:30 +0600

    about video issue solved

--- ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
+++ ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d738a6a5c31c7074d052aadbfc137afafa1e182c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa9117a7a9cb1a0cb282d7b727b19830bcc9699")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
