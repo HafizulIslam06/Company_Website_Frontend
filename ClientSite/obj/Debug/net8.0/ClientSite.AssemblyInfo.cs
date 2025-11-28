3c6a401f00055c0474782065fa4f0257cff86c8b
Hafiz <mdhafizul3131@gmail.com>
2025-11-29 00:25:21 +0600

    contact form modified like: widget added and informaton section modified

--- ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
+++ ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa9117a7a9cb1a0cb282d7b727b19830bcc9699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8529d860b7c0adacf7d36fc1cd8b024b1cd2be68")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
