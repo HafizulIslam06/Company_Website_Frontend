e078ea9200fd2d0439559eba77841d560f0a9b48
Hafiz <mdhafizul3131@gmail.com>
2025-11-29 01:04:40 +0600

    Contact page widget + Contact Information underline, logos in center done

--- ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
+++ ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa9117a7a9cb1a0cb282d7b727b19830bcc9699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8b2bd32c6ee2f657531cfb7cd8a6ab3c087271")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
