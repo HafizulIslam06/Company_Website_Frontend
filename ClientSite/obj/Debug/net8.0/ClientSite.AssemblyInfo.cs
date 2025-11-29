9c4bd7eaf6b7eb7848252355e9284240d3b9056e
Hafiz <mdhafizul3131@gmail.com>
2025-11-30 00:03:37 +0600

    footer set

--- ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
+++ ClientSite/obj/Debug/net8.0/ClientSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88040e0ede6b8da0d44994f09f7f27d85f1e3af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525d207591aed6733402dbe63eba65071e9c6439")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
